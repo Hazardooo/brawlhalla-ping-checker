1ab35f92af37cf23e1728c8f30c7b10b66bf3ca0
Pavel <Hazdoo@outlook.com>
2024-10-22 00:37:06 +0300

    first commit

---
+++ brawlhalla-ping-checker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace brawlhalla_ping_checker;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
